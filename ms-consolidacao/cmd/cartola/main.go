2cbfaf11707b315e132779f8664c29c8cc69f258
Raphael Berger <raphaelmb@gmail.com>
2022-12-06 02:13:45 -0300

    Add repo usecases

---
+++ ms-consolidacao/cmd/cartola/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/raphaelmb/imersao-cartola-consolidacao/internal/infra/db"
+	"github.com/raphaelmb/imersao-cartola-consolidacao/internal/infra/repository"
+	"github.com/raphaelmb/imersao-cartola-consolidacao/pkg/uow"
+)
+
+func main() {
+	ctx := context.Background()
+	dtb, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/cartola?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+	defer dtb.Close()
+	uow, err := uow.NewUow(ctx, dtb)
+	if err != nil {
+		panic(err)
+	}
+	registerRepositories(uow)
+}
+
+func registerRepositories(uow *uow.Uow) {
+	uow.Register("PlayerRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewPlayerRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+
+	uow.Register("MatchRepostory", func(tx *sql.Tx) interface{} {
+		repo := repository.NewMatchRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+
+	uow.Register("TeamRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewTeamRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+
+	uow.Register("MyTeamRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewMyTeamRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+}
