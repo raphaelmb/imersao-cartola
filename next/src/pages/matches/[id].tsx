07f8c5e7cdf5ecdafcfaa7ac188da11c82e1e654
Raphael Berger <raphaelmb@gmail.com>
2022-12-26 18:27:29 -0300

    Add components

---
+++ next/src/pages/matches/[id].tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const ShowMatchPage: NextPage = () => {
+  return (
+    <div>
+      <Button variant="contained">Teste</Button>
+    </div>
+  );
+};
+
+export default ShowMatchPage;
---
+++ next/src/pages/matches/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const ListMatchesPage: NextPage = () => {
+  return (
+    <div>
+      <Button variant="contained">Teste</Button>
+    </div>
+  );
+};
+
+export default ListMatchesPage;
